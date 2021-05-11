202cf9fe7ca06be70e101f5be189b3aee6f9468e
Wiwiweb <wiwiweb2@gmail.com>
2021-05-11 15:26:44 -0700

    Better tracker system, support fluids and quantity

--- control.lua
+++ control.lua
@@ -1,29 +1,41 @@
 require("scripts.tracker")
 require("scripts.gui")
 
-local function initialize_global(force_name)
-    global.forces[force_name] = {}
+local function initialize_global(force)
+    if next(force.players) ~= nil then -- Don't bother with forces without players
+        global.forces[force.name] = {
+            complete_milestones = {},
+            incomplete_milestones = milestones
+        }
+    end
 end
 
 local function clear_global(force_name)
     global.forces[force_name] = nil
 end
 
-script.on_nth_tick(120, track_item_creation)
-
 script.on_init(function()
     global.forces = {}
 
     -- Initialize for existing forces in existing save file
     for _, force in pairs(game.forces) do
-        initialize_global(force.name)
+        initialize_global(force)
     end
 end)
 
 script.on_event(defines.events.on_force_created, function(event)
-    initialize_global(event.force.name)
+    initialize_global(event.force)
 end)
 
 script.on_event(defines.events.on_forces_merged, function(event)
     clear_global(event.source_name)
 end)
+
+script.on_nth_tick(120, track_item_creation)
+
+-- Debug command
+remote.add_interface("milestones", {
+    debug_print_milestones = function()
+        game.print(serpent.block(global.forces))
+    end
+})
--- scripts/tracker.lua
+++ scripts/tracker.lua
@@ -1,10 +1,32 @@
 local misc = require("__flib__.misc")
 
-milestone_items = {
-    ["iron-gear-wheel"]=1,
-    ["automation-science-pack"]=1,
-    ["logistic-science-pack"]=1
+milestones = {
+    {type="item", name="iron-gear-wheel", quantity=1},
+    {type="item", name="iron-gear-wheel", quantity=50},
+    {type="item", name="automation-science-pack", quantity=1},
+    {type="item", name="logistic-science-pack", quantity=1},
+    {type="fluid", name="petroleum", quantity=1},
 }
+
+local function print_milestone_reached(force, milestone)
+    local human_timestamp = misc.ticks_to_timestring(milestone.completion_tick)
+    -- local item_prototype = game.item_prototypes[item]
+    -- local human_name = item_prototype.localized_name
+    local quantity_string = (milestone.quantity == 1 and "" or milestone.quantity .. " ")
+    local rich_text = "[" .. milestone.type .. "=" .. milestone.name .. "]"
+    force.print("[font=heading-1]Created the first " .. quantity_string .. rich_text .. " at [color=green]" .. human_timestamp .. "[img=quantity-time][/color]![/font]")
+    force.play_sound({path="utility/achievement_unlocked"})
+end
+
+local function check_milestone_reached(force, milestone, input_counts, milestone_index)
+    if input_counts[milestone.name] and input_counts[milestone.name] >= milestone.quantity then
+        milestone.completion_tick = game.tick
+        table.insert(global.forces[force.name].complete_milestones, milestone)
+        table.remove(global.forces[force.name].incomplete_milestones, milestone_index)
+        print_milestone_reached(force, milestone)
+    end
+end
+
 
 function track_item_creation(event)
     if event.tick == 0 then -- Skip first tick of the game where all listeners will be called
@@ -12,31 +34,23 @@ function track_item_creation(event)
     end
 
     for _, force in pairs(game.forces) do
-        if next(force.players) ~= nil and force.item_production_statistics.input_counts and next(force.item_production_statistics.input_counts) ~= nil then -- This force has players and production
-            global.forces[force.name] = global.forces[force.name] or {}
+        local global_force = global.forces[force.name]
 
-            local input_counts = force.item_production_statistics.input_counts
-            for item, threshold in pairs(milestone_items) do
-                if global.forces[force.name][item] == nil and input_counts[item] and input_counts[item] >= threshold then
-                    global.forces[force.name][item] = game.tick
-                    print_milestone_reached(force, item)
+        if global_force ~= nil and
+            force.item_production_statistics.input_counts and 
+            next(force.item_production_statistics.input_counts) ~= nil then -- This force has players and production
+                
+            local item_counts = force.item_production_statistics.input_counts
+            local fluid_counts = force.fluid_production_statistics.input_counts
+
+            for i, milestone in ipairs(global_force.incomplete_milestones) do
+                if milestone.type == "item" then
+                    check_milestone_reached(force, milestone, item_counts, i)
+                elseif milestone.type == "fluid" then
+                    check_milestone_reached(force, milestone, fluid_counts, i)
                 end
             end
         end
     end
 end
 
-remote.add_interface("milestones", {
-    debug_print_milestones = function()
-        game.print(serpent.block(global.forces))
-    end
-})
-
-function print_milestone_reached(force, item)
-    local human_timestamp = misc.ticks_to_timestring(global.forces[force.name][item])
-    -- local item_prototype = game.item_prototypes[item]
-    -- local human_name = item_prototype.localized_name
-    local rich_text = "[item=" .. item .. "]"
-    force.print("[font=heading-1]Created the first " .. rich_text .. " at [color=green]" .. human_timestamp .. "[/color]![/font]")
-    force.play_sound({path="utility/achievement_unlocked"})
-end
