ae140ab9e7cdbcd83750521bf4c12c9ba2fd3f22
Wiwiweb <wiwiweb2@gmail.com>
2023-01-24 19:23:11 -0800

    Sea block preset updates

--- presets/preset_addons.lua
+++ presets/preset_addons.lua
@@ -31,12 +31,28 @@ preset_addons = {
 
     ["Space Extension (SpaceX)"] = {
         required_mods = {"SpaceMod"},
+        forbidden_mods = {"SeaBlock"},
         milestones = {
             {type="group", name="Progress"},
-            {type="item", name="satellite",        quantity=7},
-            {type="item", name="drydock-assembly", quantity=2},
-            {type="item", name="command",          quantity=1},
-            {type="item", name="ftl-drive",        quantity=1},
+            {type="item", name="satellite",            quantity=7},
+            {type="item", name="drydock-structural",   quantity=10},
+            {type="item", name="drydock-assembly",     quantity=2},
+            {type="item", name="protection-field",     quantity=1},
+            {type="item", name="fusion-reactor",       quantity=1},
+            {type="item", name="habitation",           quantity=1},
+            {type="item", name="life-support",         quantity=1},
+            {type="item", name="command",              quantity=1},
+            {type="item", name="fuel-cell",            quantity=2},
+            {type="item", name="space-thruster",       quantity=4},
+            {type="item", name="hull-component",       quantity=10},
+            {type="technology", name="ftl-theory-A",   quantity=1},
+            {type="technology", name="ftl-theory-B",   quantity=1},
+            {type="technology", name="ftl-theory-C",   quantity=1},
+            {type="technology", name="ftl-theory-D1",  quantity=1},
+            {type="technology", name="ftl-theory-D2",  quantity=1},
+            {type="technology", name="ftl-theory-E",   quantity=1},
+            {type="technology", name="ftl-propulsion", quantity=1},
+            {type="item", name="ftl-drive",            quantity=1},
         }
     },
 
