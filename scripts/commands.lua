5a154c32281fecf615421ae42732e9866ec22678
Wiwiweb <wiwiweb2@gmail.com>
2024-10-14 12:41:42 -0700

    Remove localised strings for command descriptions
    No need to bother translators for such niche strings
    Also add descriptions to all commands

--- scripts/commands.lua
+++ scripts/commands.lua
@@ -2,21 +2,25 @@ function debug_print_forces()
   game.print(serpent.block(storage.forces))
   log(serpent.block(storage.forces))
 end
-commands.add_command("milestones-debug-print-forces", { "milestones.commands.debug-print-forces" }, debug_print_forces)
+commands.add_command("milestones-debug-print-forces",
+  "- Print the mod's internal global table, for debugging purposes.",
+  debug_print_forces)
 
 function debug_print_loaded_milestones()
   game.print(serpent.block(storage.loaded_milestones))
   log(serpent.block(storage.loaded_milestones))
 end
-commands.add_command("milestones-debug-print-loaded-milestones", { "milestones.commands.debug-print-loaded-milestones" }, debug_print_loaded_milestones)
+commands.add_command("milestones-debug-print-loaded-milestones",
+  "- Print the json of loaded milestones. Same as the \"export\" string available in settings.",
+  debug_print_loaded_milestones)
 
 function debug_set_milestones_time(command_data)
   if not command_data.parameter then return end
   local force = game.get_player(command_data.player_index).force
   local global_force = storage.forces[force.name]
   local parameters = {}
-  for k, v in string.gmatch(command_data.parameter, "([^,]+)") do
-    table.insert(parameters, k)
+  for word in string.gmatch(command_data.parameter, "([^,]+)") do -- Split comma-seperated string
+    table.insert(parameters, word)
   end
   local name = parameters[1]
   local lower_bound_tick = tonumber(parameters[2])
@@ -34,24 +38,18 @@ function debug_set_milestones_time(command_data)
   end
   game.print("Milestone not found.")
 end
-commands.add_command("milestones-debug-set-milestone-time", { "milestones.commands.debug-set-milestone-time" }, debug_set_milestones_time)
+commands.add_command("milestones-set-milestone-time",
+"- Manually set a time for a milestone. Usage: milestone_name,lower_bound_tick,tick. Example: `/milestones-set-milestone-time iron-plate,600,6000` will set the iron-plate milestone time to be between 10 seconds and 100 seconds.",
+  debug_set_milestones_time)
 
 function reinitialize_gui(command_data)
   if not command_data.player_index then return end
   reinitialize_player(command_data.player_index)
 end
-commands.add_command("milestones-reinitialize-gui", { "milestones.commands.reinitialize-gui" }, reinitialize_gui)
+commands.add_command("milestones-reinitialize-gui",
+  "- Reset the mod's internal variables related to the GUI, only for the player using the command. Try this if you experience GUI-related crashes.",
+  reinitialize_gui)
 
-function reinitialize_global()
-  for _, force in pairs(game.forces) do
-    initialize_force_if_needed(force)
-  end
-
-  for _, player in pairs(game.players) do
-    reinitialize_player(player.index)
-  end
-end
-commands.add_command("milestones-reinitialize-global", { "milestones.commands.reinitialize-global" }, reinitialize_global)
 
 function reinitialize_surfaces()
   for force_name, global_force in pairs(storage.forces) do
@@ -66,4 +64,19 @@ function reinitialize_surfaces()
     end
   end
 end
-commands.add_command("milestones-reinitialize-surfaces", { "milestones.commands.reinitialize-surfaces" }, reinitialize_surfaces)
+commands.add_command("milestones-reinitialize-surfaces",
+  "- Reset the mod's tracking of surfaces and estimate incomplete milestones. Try this if some milestones didn't trigger for you when you would expect it (and please report this issue).",
+  reinitialize_surfaces)
+
+function reinitialize_global()
+  for _, force in pairs(game.forces) do
+    initialize_force_if_needed(force)
+  end
+
+  for _, player in pairs(game.players) do
+    reinitialize_player(player.index)
+  end
+end
+commands.add_command("milestones-reinitialize-global",
+  "- Reset all of the mod's internal variables (except achieved milestones) for all players. Try this as a last resort.",
+  reinitialize_global)
