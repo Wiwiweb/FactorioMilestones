fe98e3f74d4603abe0725301ca7f0db26ef7138f
Wiwiweb <wiwiweb2@gmail.com>
2025-07-29 20:10:44 -0700

    Remove redundant or outdated presets

--- presets/preset_addons.lua
+++ presets/preset_addons.lua
@@ -54,35 +54,6 @@ preset_addons = {
         }
     },
 
-    ["Omnienergy"] = {
-        required_mods = {"omnimatter_energy"},
-        milestones = {
-            {type="group", name="Science"},
-            {type="item", name="energy-science-pack", quantity=1},
-            {type="item", name="energy-science-pack", quantity=1000, next="x10"},
-        }
-    },
-
-    ["Omniscience"] = {
-        required_mods = {"omnimatter_science"},
-        milestones = {
-            {type="group", name="Science"},
-            {type="item", name="omni-pack", quantity=1},
-            {type="item", name="omni-pack", quantity=1000, next="x10"},
-        }
-    },
-
-    ["BioIndustries Base"] = {
-        required_mods = {"Bio_Industries"},
-        milestones = {
-            {type="group", name="Bio Industries"},
-            {type="item", name="bi-bio-greenhouse", quantity=1},
-            {type="item", name="bi-bio-farm",       quantity=1},
-            {type="item", name="fertilizer",        quantity=1},
-            {type="item", name="bi-adv-fertilizer", quantity=1},
-        }
-    },
-
     ["Cargo Ships"] = {
         required_mods = {"cargo-ships"},
         milestones = {
@@ -98,30 +69,6 @@ preset_addons = {
             {type="group", name="Progress"},
             {type="item", name="spidertronmk2", quantity=1},
             {type="item", name="spidertronmk3", quantity=1},
-        }
-    },
-
-    ["Spidertron Tiers"] = {
-        required_mods = {"spidertrontiers"},
-        milestones = {
-            {type="group", name="Progress"},
-            {type="item", name="prototype_spidertron", quantity=1},
-            {type="item", name="spidertron_mkn1", quantity=1},
-            {type="item", name="spidertron_mk0", quantity=1},
-            {type="item", name="spidertron_mk2", quantity=1},
-            {type="item", name="spidertron_mk3", quantity=1},
-        }
-    },
-
-    ["Spidertron Tiers (SE+K2 fix)"] = { -- Remove this one if that issue ever gets fixed
-        required_mods = {"spidertrontiers-circulardependency"},
-        milestones = {
-            {type="group", name="Progress"},
-            {type="item", name="prototype_spidertron", quantity=1},
-            {type="item", name="spidertron_mkn1", quantity=1},
-            {type="item", name="spidertron_mk0", quantity=1},
-            {type="item", name="spidertron_mk2", quantity=1},
-            {type="item", name="spidertron_mk3", quantity=1},
         }
     },
 
@@ -152,85 +99,6 @@ preset_addons = {
             {type="group", name="Kills"},
             {type="kill", name="behemoth-explosive-biter",  quantity=1},
             {type="kill", name="explosive-leviathan-biter", quantity=1},
-        }
-    },
-
-    ["Science Pack Galore"] = {
-        required_mods = {"SciencePackGalore"},
-        milestones = {
-            {type="group", name="Science"},
-            {type="item", name="sem:spg_science-pack-1", quantity=1},
-            {type="item", name="sem:spg_science-pack-2", quantity=1},
-            {type="item", name="sem:spg_science-pack-3", quantity=1},
-            {type="item", name="sem:spg_science-pack-4", quantity=1},
-            {type="item", name="sem:spg_science-pack-5", quantity=1},
-            {type="item", name="sem:spg_science-pack-6", quantity=1},
-            {type="item", name="sem:spg_science-pack-7", quantity=1},
-            {type="item", name="sem:spg_science-pack-8", quantity=1},
-            {type="item", name="sem:spg_science-pack-9", quantity=1},
-            {type="item", name="sem:spg_science-pack-10", quantity=1},
-            {type="item", name="sem:spg_science-pack-11", quantity=1},
-            {type="item", name="sem:spg_science-pack-12", quantity=1},
-            {type="item", name="sem:spg_science-pack-13", quantity=1},
-            {type="item", name="sem:spg_science-pack-14", quantity=1},
-            {type="item", name="sem:spg_science-pack-15", quantity=1},
-            {type="item", name="sem:spg_science-pack-16", quantity=1},
-            {type="item", name="sem:spg_science-pack-17", quantity=1},
-            {type="item", name="sem:spg_science-pack-18", quantity=1},
-            {type="item", name="sem:spg_science-pack-19", quantity=1},
-            {type="item", name="sem:spg_science-pack-20", quantity=1},
-            {type="item", name="sem:spg_science-pack-21", quantity=1},
-            {type="item", name="sem:spg_science-pack-22", quantity=1},
-            {type="item", name="sem:spg_science-pack-23", quantity=1},
-            {type="item", name="sem:spg_science-pack-24", quantity=1},
-            {type="item", name="sem:spg_science-pack-25", quantity=1},
-            {type="item", name="sem:spg_science-pack-26", quantity=1},
-            {type="item", name="sem:spg_science-pack-27", quantity=1},
-            {type="item", name="sem:spg_science-pack-28", quantity=1},
-            {type="item", name="sem:spg_science-pack-29", quantity=1},
-            {type="item", name="sem:spg_science-pack-30", quantity=1},
-            {type="item", name="sem:spg_science-pack-31", quantity=1},
-            {type="item", name="sem:spg_science-pack-32", quantity=1},
-            {type="item", name="sem:spg_science-pack-33", quantity=1},
-            {type="item", name="sem:spg_science-pack-34", quantity=1},
-            {type="item", name="sem:spg_science-pack-35", quantity=1},
-            {type="item", name="sem:spg_science-pack-36", quantity=1},
-            {type="item", name="sem:spg_science-pack-1", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-2", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-3", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-4", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-5", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-6", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-7", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-8", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-9", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-10", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-11", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-12", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-13", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-14", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-15", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-16", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-17", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-18", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-19", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-20", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-21", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-22", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-23", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-24", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-25", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-26", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-27", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-28", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-29", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-30", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-31", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-32", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-33", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-34", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-35", quantity=1000, next="x10"},
-            {type="item", name="sem:spg_science-pack-36", quantity=1000, next="x10"},
         }
     },
 
@@ -302,38 +170,11 @@ preset_addons = {
         }
     },
 
-    ["Leighzer's Uranium Innovations"] = {
-        required_mods = {"leighzeruraniuminnovations"},
-        milestones = {
-            {type="group", name="Science"},
-            {type="item", name="nuclear-science-pack", quantity=1},
-            {type="item", name="nuclear-science-pack", quantity=1000, next="x10"},
-        }
-    },
-
-    ["Brass Tacks"] = {
-        required_mods = {"BrassTacks"},
-        milestones = {
-            {type="group", name="Resources"},
-            {type="item", name="zinc-plate", quantity=1},
-            {type="item", name="brass-plate", quantity=1},
-        }
-    },
-
-    ["If I Had A Nickel "] = {
-        required_mods = {"IfNickel"},
-        milestones = {
-            {type="group", name="Resources"},
-            {type="item", name="nickel-plate", quantity=1},
-            {type="item", name="invar-plate", quantity=1},
-        }
-    },
-
-    ["Cat"] = {
-        required_mods = {"petcat"},
+    ["Kitty Cat"] = {
+        required_mods = {"kittycat"},
         milestones = {
             {type="group", name="Progress"},
-            {type="item", name="cat", quantity=1},
+            {type="item", name="kcat-cat", quantity=1},
         }
     },
 }
